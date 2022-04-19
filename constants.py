d0c489dd6eaeafc6acff9e0dd9b0303a17c5693a
octarine123 <rej4m3s@gmail.com>
2022-04-19 22:20:07 +0100

    Initial commit

---
+++ constants.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# operating conditions
+P_atm = 101.1 * 10 ** 3  # Pa
+P_inlet_boost = -60.6 * 10 ** 3  # Pa
+temp_inlet = 310.0  # degK
+P_back_pressure = 1.0 * 10 ** 3  # Pa
+temp_atm = 293.0  # degK
+temp_exhaust = 878.0  # degK
+p_intake = 40.4 * 10 ** 3  # Pa
+R_air = 287.0  # Jkg^-1K^-1
+Cp = 1005.0  # Jkg^-1K^-1
+C_v = 718.0  # Jkg^-1K^-1
+gam = 1.40
+rho_air = 1.225     # kg/m^3
+TEMP_WALL = 473.0  # degK
+P_CRANK_CASE = 100.0 * 10 ** 3  # Pa
+gasoline_a_friction = 1 * 10 ** 5  # Pa
+gasoline_b_friction = 250.0
+THETA_MIN = -360.0  # deg
+THETA_DELTA = 1.0 # deg
+THETA_MAX = 360.0  # deg
+AFR = 14.7
+EFF_COMB = 0.91	# %
+Cal_Val = 42.500    # MJ/kg
+CD = 0.65
