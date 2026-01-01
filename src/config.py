440f5c8d06c41eddafa7202824fd8e258022a593
octarine123 <rej4m3s@gmail.com>
2026-01-01 00:19:07 +0000

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# operating conditions
+P_ATM = 101.1 * 10 ** 3  # Pa
+P_INLET_BOOST = -60.6 * 10 ** 3  # Pa
+TEMP_INLET = 310.0  # degK
+P_BACK_PRESSURE = 1.0 * 10 ** 3  # Pa
+TEMP_ATM = 293.0  # degK
+TEMP_EXHAUST = 878.0  # degK
+P_INTAKE = 40.4 * 10 ** 3  # Pa
+R_AIR = 287.0  # Jkg^-1K^-1
+C_P = 1005.0  # Jkg^-1K^-1
+C_v = 718.0  # Jkg^-1K^-1
+GAM = 1.40
+RHO_AIR = 1.225     # kg/m^3
+TEMP_WALL = 473.0  # degK
+P_CRANK_CASE = 100.0 * 10 ** 3  # Pa
+GAS_A_FRI = 1 * 10 ** 5  # Pa
+GAS_B_FRI = 250.0
+THETA_MIN = -360.0  # deg
+THETA_DELTA = 1.0 # deg
+THETA_MAX = 360.0  # deg
+AFR = 14.7
+EFF_COMB = 0.91	# %
+CAL_VAL = 42.500    # MJ/kg
+CD = 0.65
